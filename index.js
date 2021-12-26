903788f84c32d416642f3cd4ec587af4e6e9e6c9
Lucas Monteiro Inacio <lmsupport@outlook.com>
2021-12-26 13:57:37 -0300

    :hammer:: index port refactor

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 // eslint-disable-next-line no-console
 app.listen(port, console.log(`Listening on port: ${port}`));
